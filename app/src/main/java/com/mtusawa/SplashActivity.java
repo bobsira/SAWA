9f7a546d331074ee2698d62e52a67785a801ef00
bobsira <sbobfitz2@gmail.com>
2017-12-05 13:03:46 +0300

    Splash Screen Updates

--- app/src/main/java/com/mtusawa/SplashActivity.java
+++ app/src/main/java/com/mtusawa/SplashActivity.java
@@ -1,13 +1,40 @@
 package com.mtusawa;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends Activity {
+    private Handler mHandler;
+    private Runnable mRunnable;
+    private static final long SPLASH_DURATION = 2500L;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mHandler = new Handler();
+        mRunnable = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler.postDelayed(mRunnable,SPLASH_DURATION);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mHandler.removeCallbacks(mRunnable);
     }
 }
