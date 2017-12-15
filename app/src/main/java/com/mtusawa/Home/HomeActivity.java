8bb9dcf9007d4b732263dd7de42e0e16258bbeac
bobsira <sbobfitz2@gmail.com>
2017-12-15 12:38:46 +0300

    Configured Home Activity and Layouts for different parts

---
+++ app/src/main/java/com/mtusawa/Home/HomeActivity.java
@@ -0,0 +1,15 @@
+package com.mtusawa.Home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mtusawa.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
