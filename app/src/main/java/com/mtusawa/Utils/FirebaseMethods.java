63b3f0ffbbbc286e8f6d3182aa8c880cbaf8e6f4
bobsira <sbobfitz2@gmail.com>
2018-01-21 20:35:11 +0300

    Insert New User Data

--- app/src/main/java/com/mtusawa/Utils/FirebaseMethods.java
+++ app/src/main/java/com/mtusawa/Utils/FirebaseMethods.java
@@ -8,27 +8,36 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.auth.UserInfo;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.mtusawa.R;
 import com.mtusawa.models.User;
+import com.mtusawa.models.UserAccountSettings;
+
 
 /**
- * Created by bobsira on 1/12/18.
+ * Created by User on 6/26/2017.
  */
 
 public class FirebaseMethods {
+
     private static final String TAG = "FirebaseMethods";
 
     //firebase
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference myRef;
     private String userID;
 
     private Context mContext;
 
     public FirebaseMethods(Context context) {
         mAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        myRef = mFirebaseDatabase.getReference();
         mContext = context;
 
         if(mAuth.getCurrentUser() != null){
@@ -41,7 +50,7 @@ public class FirebaseMethods {
 
         User user = new User();
 
-        for (DataSnapshot ds: datasnapshot.getChildren()){
+        for (DataSnapshot ds: datasnapshot.child(userID).getChildren()){
             Log.d(TAG, "checkIfUsernameExists: datasnapshot: " + ds);
 
             user.setUsername(ds.getValue(User.class).getUsername());
@@ -84,4 +93,33 @@ public class FirebaseMethods {
                     }
                 });
     }
+
+
+
+    public void addNewUser(String email, String username, String description, String website, String profile_photo){
+
+        User user = new User( userID,  1,  email,  StringManipulation.condenseUsername(username) );
+
+        myRef.child(mContext.getString(R.string.dbname_users))
+                .child(userID)
+                .setValue(user);
+
+
+        UserAccountSettings settings = new UserAccountSettings(
+                description,
+                username,
+                0,
+                0,
+                0,
+                profile_photo,
+                username,
+                website
+        );
+
+        myRef.child(mContext.getString(R.string.dbname_user_account_settings))
+                .child(userID)
+                .setValue(settings);
+
+    }
+
 }
