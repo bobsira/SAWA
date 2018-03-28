60c30224acfcbb1db11072abe7411745a73d255d
bobsira <sbobfitz2@gmail.com>
2018-03-29 00:59:54 +0300

    Setup Gallery and Photo Fragments

---
+++ app/src/main/java/com/mtusawa/Share/GalleryFragment.java
@@ -0,0 +1,27 @@
+package com.mtusawa.Share;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.mtusawa.R;
+
+/**
+ * Created by bobsira on 3/28/18.
+ */
+
+public class GalleryFragment extends Fragment{
+    private static final String TAG = "GalleryFragment";
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_gallery, container, false);
+        Log.d(TAG, "onCreateView: started.");
+
+        return view;
+    }
+}
--- app/src/main/java/com/mtusawa/Share/ShareActivity.java
+++ app/src/main/java/com/mtusawa/Share/ShareActivity.java
@@ -39,22 +39,22 @@ public class ShareActivity extends AppCompatActivity {
         }else{
             verifyPermissions(Permissions.PERMISSIONS);
         }
-        setupBottomNavigationView();
+        //setupBottomNavigationView();
     }
 
     private void setupViewPager(){
         SectionsPagerAdapter adapter =  new SectionsPagerAdapter(getSupportFragmentManager());
-//        adapter.addFragment(new GalleryFragment());
-//        adapter.addFragment(new PhotoFragment());
-//
-//        mViewPager = (ViewPager) findViewById(R.id.container);
-//        mViewPager.setAdapter(adapter);
-//
-//        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabsBottom);
-//        tabLayout.setupWithViewPager(mViewPager);
-//
-//        tabLayout.getTabAt(0).setText(getString(R.string.gallery));
-//        tabLayout.getTabAt(1).setText(getString(R.string.photo));
+        adapter.addFragment(new GalleryFragment());
+        adapter.addFragment(new PhotoFragment());
+
+        mViewPager = (ViewPager) findViewById(R.id.viewpager_container);
+        mViewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabsBottom);
+        tabLayout.setupWithViewPager(mViewPager);
+
+        tabLayout.getTabAt(0).setText(getString(R.string.gallery));
+        tabLayout.getTabAt(1).setText(getString(R.string.photo));
 
     }
 
