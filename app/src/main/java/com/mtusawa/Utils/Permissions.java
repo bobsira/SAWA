e7c636b4ae4938546e80b0930433bb34d0d0ffb2
bobsira <sbobfitz2@gmail.com>
2018-03-28 17:32:26 +0300

    Verifying Permissions for Sharing

---
+++ app/src/main/java/com/mtusawa/Utils/Permissions.java
@@ -0,0 +1,15 @@
+package com.mtusawa.Utils;
+
+import android.Manifest;
+
+/**
+ * Created by bobsira on 3/28/18.
+ */
+
+public class Permissions {
+    public static final String[] PERMISSIONS = {
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+}
